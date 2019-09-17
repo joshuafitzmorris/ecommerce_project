1608061c223edbb63fe66ac6bb8c4168833715ca
Joshua Fitz-Morris <jfitzmor@sfu.ca>
2019-09-17 16:53:55 -0700

    Slugfield

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,9 @@
 import random
 import os
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -41,9 +44,10 @@ class ProductManager(models.Manager):
         return None
 
 class Product(models.Model):
-    title           = models.CharField(max_length=120)
+    title           = models.CharField(max_length=200)
+    slug            = models.SlugField(blank=True, unique=True)
     description     = models.TextField()
-    price           = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    price           = models.DecimalField(decimal_places=2, max_digits=20, default=79.99)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     featured        = models.BooleanField(default=False)
     active          = models.BooleanField(default=True)
@@ -51,8 +55,24 @@ class Product(models.Model):
 
     objects = ProductManager()
 
+    def get_absolute_url(self):
+        return "/products{slug}/".format(slug=self.slug)
+
     def __str__(self):
         return self.title
 
     def __unicode__(self):
         return self.title
+
+
+def product_pre_save_receiver(sender,instance,*args,**kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
+
+
+
+
+
+
--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,3 @@
-# from django.views import ListView
 from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
@@ -19,19 +18,10 @@ class ProductFeaturedDetailView(DetailView):
     queryset = Product.objects.all().featured()
     template_name = "products/featured-detail.html"
 
-    # def get_queryset(self, *args, **kwargs):
-    #     request = self.request
-    #     return Product.objects.featured()
-
-
 
 class ProductListView(ListView):
     template_name = "products/list.html"
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
@@ -46,14 +36,34 @@ def product_list_view(request):
     return render(request, "products/list.html", context)
 
 
+
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist:
+            raise Http404("Not found..")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404("Uhhmmm ")
+        return instance
+
+
+
 class ProductDetailView(DetailView):
-    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
-        # context['abc'] = 123
         return context
 
     def get_object(self, *args, **kwargs):
@@ -63,11 +73,6 @@ class ProductDetailView(DetailView):
         if instance is None:
             raise Http404("Product doesn't exist")
         return instance
-
-    # def get_queryset(self, *args, **kwargs):
-    #     request = self.request
-    #     pk = self.kwargs.get('pk')
-    #     return Product.objects.filter(pk=pk)
 
 
 def product_detail_view(request, pk=None, *args, **kwargs):
