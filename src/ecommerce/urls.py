30fd4c5d3a5d19fbc23b80f89ce8f4d2228a4e71
Joshua Fitz-Morris <jfitzmor@sfu.ca>
2019-11-04 18:45:32 -0800

    search app created - tags app included to compliment searching

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -1,4 +1,3 @@
-Learn more or give us feedback
 """ecommerce URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
