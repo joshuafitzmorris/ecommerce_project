b7c9c2217b50637b6858924f98950e3e233da314
Joshua Fitz-Morris <jfitzmor@sfu.ca>
2019-11-10 22:50:12 -0800

    stripe integration

--- src/billing/models.py
+++ src/billing/models.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save, post_save
 
 from accounts.models import GuestEmail
 User = settings.AUTH_USER_MODEL
 
+import stripe
+stripe.api_key = "sk_test_SiPaE5LzttrsrT6fdxXwXbXa00ykr9dtCr"
 
 class BillingProfileManager(models.Manager):
     def new_or_get(self, request):
@@ -31,6 +33,7 @@ class BillingProfile(models.Model):
     active      = models.BooleanField(default=True)
     update      = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
+    customer_id = models.CharField(max_length=120, null=True, blank=True)
     # customer_id in Stripe or Braintree
 
     objects = BillingProfileManager()
@@ -38,6 +41,14 @@ class BillingProfile(models.Model):
     def __str__(self):
         return self.email
 
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+    if not instance.customer_id and instance.email:
+        print("API request send to stripe")
+        customer = stripe.Customer.create(
+            email = instance.email
+        )
+        instance.customer_id = customer.id
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
     if created and instance.email:
--- src/billing/views.py
+++ src/billing/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+import stripe
+stripe.api_key = "sk_test_SiPaE5LzttrsrT6fdxXwXbXa00ykr9dtCr"
+STRIPE_PUB_KEY = 'pk_test_iWK0KiaMkmLLLhzxJldzqLpy00hRxvSgRi'
+
+def payment_method_view(request):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, 'billing/payment-method.html', {"publish_key" : STRIPE_PUB_KEY})
